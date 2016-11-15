303c863d8acd29f6d62f9b19a47fed04380e2a97
Olivier MICHAUD <o.michaud@servicemagic.eu>
2016-11-15 08:24:00 +0100

    checkParams

--- bin/ImageLibraryProcessor.php
+++ bin/ImageLibraryProcessor.php
@@ -9,17 +9,23 @@ use Symfony\Component\Filesystem\Filesystem;
 
 class ImageLibraryProcessor{
 	static $sLibraryPath;
+	static $aTypeExtensions = [
+		'jpg' =>'jpg',
+		'gif' =>'gif',
+		'png' =>'png',
+		'jpeg'=>'jpg'
+	];
 
 	public static function run($sLibraryPath){
 		self::$sLibraryPath = realpath($sLibraryPath);
-		$action		= $_GET['action'];
-		$aSize		= explode('x',$_GET['size']);
-		$filename	= $_GET['file'];
-		$type		= $_GET['type'];
 		$path		= $_GET['path'];
-		$arg		= array_key_exists('arg',$_GET)?$_GET['arg']:null;
-		$sMethod	= 'action'.ucfirst($action);
+		$filename	= $_GET['file'];
+		$aSize		= explode('x',$_GET['size']);
+		$arg		= trim($_GET['arg'])==''?[]:explode('/',$_GET['arg']);
+		$type		= self::$aTypeExtensions[strtolower($_GET['type'])];
+		$sMethod	= 'action'.ucfirst($_GET['action']);
 		$sFullfilename = realpath(self::$sLibraryPath.'/'.$filename);
+
 		if (!file_exists($sFullfilename)){
 			header('HTTP/1.0 404 Picture Not Found');
 			die();
@@ -29,17 +35,14 @@ class ImageLibraryProcessor{
 			header('HTTP/1.0 403 Not allowed');
 			die();
 		}
+
 		if(in_array($sMethod, get_class_methods('ImageLibraryProcessor'))){
 			$imagine = new Imagine();
 			/** @var $image Image */
 			$image = $imagine->open($sFullfilename);
-			$image = self::$sMethod($image,new Box($aSize[0], $aSize[0]),$arg);
+			$image = self::$sMethod($image,new Box($aSize[0], $aSize[1]),$arg);
 			self::httpRender($image,$type,$path,$filename);
 		}
-	}
-
-	private function actionResize(Image $image,BoxInterface $size,$arg){
-		return $image->thumbnail($size, ImageInterface::THUMBNAIL_INSET);
 	}
 
 	private function httpRender(ImageInterface $image,$type,$path,$filename){
@@ -53,6 +56,68 @@ class ImageLibraryProcessor{
 		$image->save(self::$sLibraryPath.'/'.$sExportPath.'/'.$filename);
 		$image->show($type);
 	}
+
+	private function checkParams(array $aParams,BoxInterface $size,array $aTemplate){
+		$args = [];
+		$aErrors=[];
+		if($size->getWidth() >2048){
+			$aErrors[]='too large';
+		}
+		if($size->getHeight() >2048){
+			$aErrors[]='too high';
+		}
+		foreach($aTemplate as $argTtpl){
+			$args[$argTtpl['name']]=$argTtpl['default'];
+			foreach($aParams as $k=>$subArg){
+				$rgx='!'.$argTtpl['rgx'].'!';
+				$match=[];
+				if (preg_match($rgx,$subArg,$match) && $args[$argTtpl['name']] = preg_replace($rgx,$argTtpl['rpl'],$subArg)){
+					unset($aParams[$k]);
+					continue;
+				}
+			}
+		}
+
+		if(count($aParams)>0){
+			$aErrors[] = 'Unknown parameters : '.implode(',',$aParams);
+		}
+
+		if(count($aErrors)>0){
+			$sError=implode('/',$aErrors);
+			header('HTTP/1.0 500 '.$sError);
+			die('<h1>'.$sError.'</h1>');
+		}
+		return $args;
+	}
+
+	/**
+	 *
+	 * @param Image $image
+	 * @param BoxInterface $size
+	 * @param string $arg
+	 * @return \Imagine\Imagick\Image|\Imagine\Image\ImageInterface
+	 */
+	private function actionThumbnail(Image $image,BoxInterface $size,$args=[]){
+		$args=self::checkParams($args,$size,[
+			[
+				'name'=>'mode',
+				'rgx'=>'mode(I|O)',
+				'rpl'=>'$1',
+				'default'=>'I'
+			]
+		]);
+
+		switch ($args['mode']){
+			case 'I':
+				$mode=ImageInterface::THUMBNAIL_INSET;
+			break;
+			case 'O':
+				$mode=ImageInterface::THUMBNAIL_OUTBOUND;
+			break;
+		}
+		return $image->thumbnail($size,$mode);
+	}
+
 }
 
 ImageLibraryProcessor::run(realpath(dirname(__FILE__).'/../library/'));
